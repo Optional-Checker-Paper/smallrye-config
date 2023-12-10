0f06a00411e3ff3631e2833cf707e12b91c852be
Michael Ernst <mernst@cs.washington.edu>
2023-12-09 17:17:56 -0800

    Reduce size of diffs

--- utils/events/src/main/java/io/smallrye/config/events/ChangeEvent.java
+++ utils/events/src/main/java/io/smallrye/config/events/ChangeEvent.java
@@ -12,13 +12,12 @@ public class ChangeEvent implements Serializable {
 
     private final Type type;
     private final String key;
-
-    @SuppressWarnings("optional:optional.field") // optional-field : use of optional as a field
+    @SuppressWarnings("optional:optional.field") // optional-field
     private final Optional<String> oldValue;
     private final String newValue;
     private final String fromSource;
 
-    @SuppressWarnings("optional:optional.parameter") // optional-parameter : use of optional as a parameter
+    @SuppressWarnings("optional:optional.parameter") // optional-parameter
     public ChangeEvent(Type type, String key, Optional<String> oldValue, String newValue, String fromSource) {
         this.type = type;
         this.key = key;
